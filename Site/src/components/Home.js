8fa1a95d0e7c5e38a57aa4b18eb375c79303c664
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-05-15 22:27:53 +0900

    2차 초안

--- Site/src/components/Home.js
+++ Site/src/components/Home.js
@@ -1,5 +1,6 @@
 import { Carousel } from "antd";
 import { Divider } from "antd";
+import { Card } from "antd";
 import "../styles/Home.css";
 import A from "../images/Homeimg.webp";
 import {
@@ -9,9 +10,6 @@ import {
 } from "@ant-design/icons";
 
 const contentStyle = {
-  height: "500px",
-  color: "#000205",
-  lineHeight: "160px",
   textAlign: "center",
 };
 function Home() {
@@ -19,31 +17,48 @@ function Home() {
     <div>
       <Carousel autoplay delayLength={2}>
         <div className="Home">
-          <div className="font">
-            <h1 style={contentStyle}>Study With Me?</h1>
+          <p id="fontp">
+            <b>All-In-One</b>
+          </p>
+          <h1 style={contentStyle} className="font">
+            Study With Me?
+          </h1>
+          <div className="font-t">
+            <p>
+              <b>Algorithms</b>
+              <br />
+              <b>Study</b>
+              <br />
+              <b>Flatform</b>
+            </p>
+          </div>
+          <div className="fontq-q">
+            <p className="fontq">
+              <b>Somebody needs you, Don't give up</b>
+            </p>
           </div>
         </div>
-        <div className="Home2">
+        {/* <div className="Home2">
           <div className="font2">
             <h1 style={contentStyle}>
               A complete platform for <b>Algorithms</b>
             </h1>
           </div>
-        </div>
+        </div> */}
       </Carousel>
       <Divider></Divider>
       <img src={A} className="A" />
       <div className="H">
-        <SmileTwoTone />
-        <HeartTwoTone twoToneColor="#eb2f96" />
-        <CheckCircleTwoTone twoToneColor="#52c41a" />
-        <h1 className="H1">함께 더 많은 목표를 달성</h1>
+        <h1 className="H1">
+          <b>함께 더 많은 목표를 달성</b>
+        </h1>
         <p className="P1">
-          <b>All-in-One</b> <br></br> 원격으로 지식을 공유하고 공동작업 할 수
-          있는 플랫폼
+          <b>All-in-One</b> <br></br>{" "}
+          <b>원격으로 지식을 공유하고 공동작업 할 수 있는 플랫폼</b>
         </p>
         <button>바로 시작하기!</button>
       </div>
+      <Divider></Divider>
     </div>
   );
 }
