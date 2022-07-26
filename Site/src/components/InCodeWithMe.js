c7e1ce4b7dcb632b9a31ab85a3e7cdf82b69e279
wjdtmdgh <wjdtmdgh926@naver.com>
2022-07-26 22:15:30 +0900

    WebRTC 추가

--- Site/src/components/InCodeWithMe.js
+++ Site/src/components/InCodeWithMe.js
@@ -1,17 +1,24 @@
 import React, {useState} from "react";
 import "../styles/InCodeWithMe.css";
 import MonacoEditor from 'react-monaco-editor';
-import {Select} from "antd";
+import {Col, Row, Select} from "antd";
 import {Option} from "antd/es/mentions";
+import JoinMeeting from "./JoinMeeting";
+import Code from "./Code";
 
 function InCodeWithMe() {
   const [code, setCode] = useState("")
+  const [fontSizeList, setFontSizeList] = useState([10, 15, 20, 25])
   const [fontSize, setFontSize] = useState(10)
   const [language, setLanguage] = useState("javascript")
 
   const options = {
     copyWithSyntaxHighlighting: true,
     selectOnLineNumbers: true,
+    autoIndent: "full",
+    bracketPairColorization: true,
+    occurrencesHighlight: true,
+    language: language,
     fontSize: fontSize
   };
 
@@ -25,10 +32,12 @@ function InCodeWithMe() {
   }
 
   const onFontSizeChange = (newValue, e) => {
+    console.log(newValue);
     setFontSize(newValue)
   }
 
   const onLanguageChange = (newValue, e) => {
+    console.log(newValue);
     setFontSize(newValue)
   }
 
@@ -43,16 +52,21 @@ function InCodeWithMe() {
       <Option value="javascript">javascript</Option>
       <Option value="java">java</Option>
     </Select>
-    <MonacoEditor
-      width="800"
-      height="600"
-      language={language}
-      theme="vs-dark"
-      value={code}
-      options={options}
-      onChange={onChange}
-      editorDidMount={editorDidMount}
-    />
+    <Row>
+      <Col span={7}>
+        <MonacoEditor
+          language={language}
+          theme="vs-dark"
+          value={code}
+          options={options}
+          onChange={onChange}
+          editorDidMount={editorDidMount}
+        />
+      </Col>
+      <Col span={1}>
+        <JoinMeeting/>
+      </Col>
+    </Row>
   </div>
 }
 export default InCodeWithMe;
---
+++ Site/src/components/JoinMeeting.js
@@ -0,0 +1,49 @@
+export default function JoinMeeting(){
+
+  const constraints = window.constraints = {
+    audio: false,
+    video: true
+  };
+
+  function handleSuccess(stream) {
+    const video = document.querySelector('video');
+    const videoTracks = stream.getVideoTracks();
+    console.log('Got stream with constraints:', constraints);
+    console.log(`Using video device: ${videoTracks[0].label}`);
+    window.stream = stream;
+    video.srcObject = stream;
+  }
+
+  function handleError(error) {
+    if (error.name === 'OverconstrainedError') {
+      const v = constraints.video;
+      errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`);
+    } else if (error.name === 'NotAllowedError') {
+      errorMsg('카메라와 마이크를 사용할 수 있는 권한이 없습니다.');
+    }
+    errorMsg(`getUserMedia error: ${error.name}`, error);
+  }
+
+  function errorMsg(msg, error) {
+    const errorElement = document.querySelector('#errorMsg');
+    errorElement.innerHTML += `<p>${msg}</p>`;
+    if (typeof error !== 'undefined') {
+      console.error(error);
+    }
+  }
+
+  async function init(e) {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      handleSuccess(stream);
+      e.target.disabled = true;
+    } catch (e) {
+      handleError(e);
+    }
+  }
+
+  return <div id="container">
+    <video autoPlay playsInline></video>
+    <button onClick={init}>Open camera</button>
+  </div>
+}
