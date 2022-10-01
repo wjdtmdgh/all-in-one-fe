b8292953310d54d06fecdc314cc1360f7940455e
wjdtmdgh <wjdtmdgh926@naver.com>
2022-10-01 21:12:59 +0900

    사용하지 않는 코드 제거

--- Site/src/components/auth/SignOut.js
+++ Site/src/components/auth/SignOut.js
@@ -1,11 +1,8 @@
-import {useNavigate} from "react-router-dom";
 import {useEffect} from "react";
 
 export default function SignOut() {
   localStorage.clear()
-  const navigate = useNavigate();
   useEffect(() => {
-    // navigate("/sign-in");
     window.location.replace("/sign-in")
   }, [])
 
